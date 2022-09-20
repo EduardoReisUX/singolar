578d7244ec9ca28d2e8029b55497849a4732c76a
Eduardo Fernandes <ducabruno09@hotmail.com>
2022-09-20 16:14:29 -0400

    refactor: better typing the themes values

--- src/@types/styled-components/styled.d.ts
+++ src/@types/styled-components/styled.d.ts
@@ -3,42 +3,40 @@ import "styled-components"
 declare module "styled-components" {
   export interface DefaultTheme {
     colors: {
-      black: string
-      white: string
-      link: string
-      cta: string
-      success: string
-      error: string
-      gray: string
+      black: "#0F172A"
+      white: "#FFFFFF"
+      link: "#3B82F6"
+      cta: "#4338CA"
+      success: "#34D399"
+      error: "#FB7185"
+      gray: "#4B5563"
     }
-
     fonts: {
-      logo: string
-      normal: string
-      serif: string
+      logo: "'Comfortaa', cursive"
+      normal: "'Roboto', sans-serif"
+      serif: "'Source Serif Pro', serif"
     }
-
     fontSizes: {
-      logo: string
-      "2xl": string
-      xl: string
-      lg: string
-      base: string
-      sm: string
-      xs: string
+      logo: "2.25rem"
+      "2xl": "1.5rem"
+      xl: "1.25rem"
+      lg: "1.125rem"
+      base: "1rem"
+      sm: "0.875rem"
+      xs: "0.75rem"
     }
     fontWeight: {
-      normal: string
-      bold: string
+      normal: "normal"
+      bold: "bold"
     }
     lineHeight: {
-      logo: string
-      "2xl": string
-      xl: string
-      lg: string
-      base: string
-      sm: string
-      xs: string
+      logo: "36px"
+      "2xl": "26.4px"
+      xl: "24px"
+      lg: "24.3px"
+      base: "22.4px"
+      sm: "20.3px"
+      xs: "18px"
     }
   }
 }
